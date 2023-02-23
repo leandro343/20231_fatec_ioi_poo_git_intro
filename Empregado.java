3af3f185634e701c7825a3fb8de1b088bccd8318
Aluno <leandro.silva343@fatec.sp.gov.br>
2023-02-23 17:26:09 -0300

    Cálculo de salario efetuado de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,19 +1,35 @@
-public class Empregado(
+public class Empregado{
     private String nome;
     private int idade;
-
-    public String getNome(){
-        return nome;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    public double calculaSalario (){
+    if (tipo == 1){
+    return salario;
     }
-
-    public void setNome(String nome){
-        this.nome = nome;        
-
+    else if (tipo == 2){
+    //comissão varia de 0 a 1, é um percentual sobre o salário
+    return salario + salario * comissao;
     }
-    public String getIdade(){
-        return idade;
-    } 
-
-    public void setIdade(String idade){
-        this.idade = idade;
+    else if (tipo == 3){
+    return salario + bonus;
     }
+    else{
+    return 0.;
+    }
+    }
+    public String getNome (){
+    return this.nome;
+    }
+    public void setNome (String nome){
+    this.nome = nome;
+    }
+    public int getIdade (){
+    return this.idade;
+    }
+    public void setIdade (int idade){
+    this.idade = idade;
+    }
+   }
