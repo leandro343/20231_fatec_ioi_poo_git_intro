78385ac59c4152585de8b429aadcdfb1826fc03c
Aluno <aluno@IPIRANGA.FATEC>
2023-02-23 16:12:12 -0300

    Commit inicial. Classe Empregado criada.

---
+++ Empregado.java
@@ -0,0 +1,22 @@
+public class Empregado(
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return nome;
+    }
+
+
+    public void setNome(String nome){
+        this.nome = nome;        
+
+    }
+    public String getIdade(){
+        return idade;
+    } 
+
+    public void setIdade(String nome){
+        this.idade = idade;
+    }
+
+)
